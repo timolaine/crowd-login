ab439be55c1c1d77e6593154cd28bd0e42e5007a
Ari Gesher <ari@gesher.net>
2012-11-26 17:47:32 -0800

    Checkpoint as further functions are implemented

--- Crowd-REST.php
+++ Crowd-REST.php
@@ -17,7 +17,31 @@ class CrowdREST {
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml")); 
 		curl_setopt($curl, CURLOPT_POSTFIELDS,$post_body);
-		return curl_exec($curl);
+		$rc = curl_exec($curl);
+		$info = curl_getinfo($curl);
+		return array("response" => $rc, "metadata" => $info);
+	}
+
+	function curl_logerror($rc, $msg_prefix = "curl error") {
+		$http_response_code = $rc['metadata'][CURLINFO_HTTP_CODE];
+
+		if($http_response_code != 200) {
+			error_log("${msg_prefix}:\n" . $rc['response']);
+			return false;
+		}
+
+		return true;
+	}
+
+	function crowd_xml_logerror($rc, $msg_prefix = "error in xml response") {
+		// got back a valid XML response (hopefully)
+		$xmlResponse = new SimpleXMLElement($rc['response']);
+		if($xmlResponse[0]->getName() == "error") {
+			error_log("${msg_prefix}\n" . $rc['response']);
+			return null;
+		}
+
+		return $xmlResponse;
 	}
 
 	function isSSOEnabled() {
@@ -37,16 +61,22 @@ class CrowdREST {
 	}
 
 	function simpleAuth($username, $password) {
-		$xmlBody = generateSimpleAuthXML($username, $password);
+		$xmlBody = generateSimpleAuthXML($password);
 		$rc = $curlPost("/authentication", array("username" => $username),$xmlBody);
 
-		$xmlResponse = new SimpleXMLElement($rc);
-		if($xmlResponse[0]->getName() == "error") {
+		// check to make sure we got a 200 and response from the server
+		if(curl_logerror($rc,"Error in performing simple authentication:\n")){
 			return false;
 		}
 
-		if($xmlResponse[0]->getName() == "user") {
-			return ($xmlResponse[0]-getAttr('username') == $username);
+		// got back a valid XML response (hopefully)
+		$xmlResponse = crowd_xml_logerror($rc,"Error returned in Crowd XML response");
+		if($xmlResponse) {
+			if($xmlResponse[0]->getName() == "user") {
+				return ($xmlResponse[0]-getAttr('username') == $username);
+			} else {
+				error_log("Got unexpected Crowd XML response to auth query:\n" . $rc['response']);
+			}
 		}
 
 		return false;
@@ -56,7 +86,45 @@ class CrowdREST {
 		return false;
 	}
 
-	functiopn generateSimpleAuthXML($username, $password) {
+	function userIsInGroup($username, $groupname) {
+		$rc = curlPost("/user/group/nested",array("username" => $username, "groupname" => $groupname));
+
+		$http_response_code = $rc['metadata']['CURLINFO_HTTP_CODE'];
+
+		if($http_response_code == 200) {
+			// user belongs to group
+			return true;
+		} elseif ($http_response_code == 404) {
+			// user not in group
+			return false;
+		} else {
+			// some other error
+			curl_logerror($rc, "Error while confirming membership of '${username}' in group '${groupname}'");
+			return false;
+		}
+	}
+
+	function getUserInfo($username) {
+		$rc = curlPost('/user', array('username' => $username));
+
+		if (curl_logerror($rc,"Error while retrieving user info for username '${username}'")){
+			return null;
+		}
+
+		// got back a valid XML response (hopefully)
+		$xmlResponse = crowd_xml_logerror($rc,"Error returned in Crowd XML response");
+		if($xmlResponse) {
+			if($xmlResponse[0]->getName() == "user") {
+				// TODO continue here
+				$firstname = $xmlResponse[1]->getValue()				
+				return ($xmlResponse[0]-getAttr('username') == $username);
+			} else {
+				error_log("Got unexpected Crowd XML response to auth query:\n" . $rc['response']);
+			}
+		}
+	}
+
+	function generateSimpleAuthXML($password) {
 		$document = new DOMDocument("1.0","UTF-8");
 		$password = $document->appendChild($document->createElement("password"));
 		$value = $password->appendChild($document->createElement("value"));
@@ -65,6 +133,5 @@ class CrowdREST {
 
 		return $document->asXML();
 	}
-}
 
 ?>
